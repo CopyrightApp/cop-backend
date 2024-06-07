576a07ee8792ead2d6417fffa27b3631b749e27c
Santi4go32 <quierocarne40@gmail.com>
2024-06-07 09:59:56 -0500

    suggestion prompt

--- routes/verify.js
+++ routes/verify.js
@@ -14,14 +14,19 @@ router.post('/check', async (req, res) => {
     const userMessage = req.body.transcription;
     console.log("XD", userMessage)
 
-    const prompt = ' Eres un gran conocedor de canciones. Sabes de las más populares del medio. Dime, esta letra a cuál canción pertenece: \n'
-    //const inicio = 'Yo escribí esta letra, si ya pertenece a una canción existente o es similar a alguna. Ayúdame a cambiarla para que ya no incumpla las normas: \n'
+    const prompt = '\n\nEres un gran conocedor de canciones. Sabes de las más populares del medio. Dime, esta letra a cuál canción pertenece \n'
+    const inicio = 'Yo escribí esta letra: \n'
+    const aclaracion = 'Si conoces la canción responde así: Esta letra ya existe o es similar a *inserte la canción a la cual pertenece.'
+    const aclaracion2 = '\nSi no conoces la canción y solamente cuando no conoces la canción, responde: Esta letra parece ser original. y agrégale sugerencias tipo Sin embargo, ten en cuenta que yo no conozco todas las canciones y deberías indagar más para evitar el plagio.'
+    const final = '\nAdemás, si la conoces y solamente cuando conoces la canción. Dame dos opciones separadas distintivamente con un asterisco(*). Cada una de las opciones va a tener sugerencias que consideres podrían ayudar a mejorar la letra o hacer que sea distinta a la de la canción encontrada.'
+    const final2 = '\nQue las opciones sean detalladas y que no sean tan cortas.'
+    const conclusion = '\n\nFavor responde únicamente como lo solicité, no agregue nada más.'
 
     try {
         const response = await openai.chat.completions.create({
           model: "gpt-3.5-turbo",
           messages: [
-            { role: "user", content: prompt + userMessage }
+            { role: "user", content: inicio + userMessage + prompt + aclaracion + final + aclaracion2 + final2 + conclusion}
           ]
         });
         res.status(200).json(response);
