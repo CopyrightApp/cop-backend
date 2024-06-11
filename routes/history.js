69ab5df59303afc3d0378229db74cc275b31eaec
Sebastian Caicedo <sdrivert@hotmail.com>
2024-06-11 11:16:16 -0500

    Se ha agregado los modelos y controllers de history

--- routes/history.js
+++ routes/history.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const User = require("../models/History");
+const History = require("../models/History");
 
 // Middleware para verificar el JWT en las solicitudes
 function verificarJWT(req, res, next) {
@@ -16,7 +16,6 @@ function verificarJWT(req, res, next) {
     if (err) {
       return res.status(401).json({ mensaje: "Token inválido" });
     }
-    console.log("Lo decodificado es ", decoded);
     if (decoded.googleId) {
       req.googleId = decoded.googleId;
     } else if (decoded.email) {
@@ -26,17 +25,46 @@ function verificarJWT(req, res, next) {
     next();
   });
 }
+//@desc Ruta para agregar un elemento al historial
+//@route POST /history
+router.post("/", verificarJWT, async (req, res) => {
+  if (req.googleId) {
+    const googleId = req.googleId;
+    let history = await History.findOne({ userId: googleId });
+    if (history) {
+      const newValue = { details: req.body.details };
+      history.values.push(newValue);
+      await history.save();
+      return res.status(200).json({ history: history.values });
+    }
+    return res.status(400).json({ message: "User history can not be found" });
+  } else if (req.email) {
+    const email = req.email;
+    let history = await History.findOne({ userId: email });
+    if (history) {
+      const newValue = { details: req.body.details };
+      history.values.push(newValue);
+      await history.save();
+      return res.status(200).json({ history: history.values });
+    }
+    return res.status(400).json({ message: "User history can not be found" });
+  } else {
+    return res.status(400).json({ message: "JWT can not be resolved" });
+  }
+});
 
-// Ruta protegida que requiere el JWT para acceder
-router.get("/", verificarJWT, async(req, res) => {
+//@desc Ruta para obtener todo el historial de un su
+//@route GET /history
+router.get("/", verificarJWT, async (req, res) => {
   // El googleId del usuario está disponible en req.googleId
   if (req.googleId) {
     const googleId = req.googleId;
-    let history = await History.findOne(googleId)
-    return res.status(200).json({ googleId: googleId });
+    let history = await History.findOne({ userId: googleId });
+    return res.status(200).json({ history: history.values });
   } else if (req.email) {
     const email = req.email;
-    return res.status(200).json({ email: email });
+    let history = await History.findOne({ userId: email });
+    return res.status(200).json({ history: history.values });
   } else {
     return res.status(400).json({ message: "JWT can not be resolved" });
   }
